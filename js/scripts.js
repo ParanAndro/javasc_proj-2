4b6506477acc87fa97fc3fd8dd204a0629b93d03
Radek Izdebski <radek1izdebski@gmail.com>
2018-01-09 10:52:43 +0100

    Add comments in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
-// scripts.js
+// scripts.js - linear comment;
 var a = 9,
 	h = 12,
 	triangleArea = a * h / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+/*
+block comment about area of triangle;
+*/
+//second linear comment;
